c25fa018869409c4e0e2ff0af125235118dd48d9
JanTigeros <59819725+JanTigeros@users.noreply.github.com>
2022-06-16 00:22:32 +0200

    Small fixes on visual look

--- competition.php
+++ competition.php
@@ -20,7 +20,7 @@
     <div class="main">
         <div class="container">
             <div class="title">
-                <h1>O Roboligi</h1>
+                <h1>Tekmovanje</h1>
             </div>
             <div class="text">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
--- footer.php
+++ footer.php
@@ -20,13 +20,12 @@
     </div> -->
     <div class="footer-basic">
         <footer>
-            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
             <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                <li class="list-inline-item"><a href="index.php">Domov</a></li>
+                <li class="list-inline-item"><a href="news.php">Novice</a></li>
+                <li class="list-inline-item"><a href="rules.php">Pravila</a></li>
+                <li class="list-inline-item"><a href="about.php">O Nas</a></li>
+                <li class="list-inline-item"><a href="competition.php">Tekmovanje</a></li>
             </ul>
             <p class="copyright">Jan Sajtl © 2022</p>
         </footer>
